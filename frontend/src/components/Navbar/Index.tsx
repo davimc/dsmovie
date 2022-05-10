1eb31e36f75b1533e32453f5e5f76153ee207e68
davimc <davimatosc@hotmail.com>
2022-05-10 15:45:15 -0300

    Navbar

--- frontend/src/components/Navbar/Index.tsx
+++ frontend/src/components/Navbar/Index.tsx
@@ -3,21 +3,19 @@ import './styles.css'
 
 function Navbar() {
     return (
-        <>
-            <header>
-                <nav className="container">
-                    <div className="dsmovie-nav-content">
-                        <h1>DSMovie</h1>
-                        <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
-                            <div className="dsmovie-contact-container">
-                                <GithubIcon />
-                                <p className="dsmovie-contact-link">/devsuperior</p>
-                            </div>
-                        </a>
-                    </div>
-                </nav>
-            </header>
-        </>
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
     );
 }
 
