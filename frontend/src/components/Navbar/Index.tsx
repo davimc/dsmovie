34f02df944105d7454b8190942c14d23eee5bbff
davimc <davimatosc@hotmail.com>
2022-05-10 15:38:47 -0300

    Navbar

--- frontend/src/components/Navbar/Index.tsx
+++ frontend/src/components/Navbar/Index.tsx
@@ -8,10 +8,10 @@ function Navbar() {
                 <nav className="container">
                     <div className="dsmovie-nav-content">
                         <h1>DSMovie</h1>
-                        <a  href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                        <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
                             <div className="dsmovie-contact-container">
                                 <GithubIcon />
-                                <p className="dsmovie-contact-link">/DevSuperior</p>
+                                <p className="dsmovie-contact-link">/devsuperior</p>
                             </div>
                         </a>
                     </div>
