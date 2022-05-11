2be4ef91c29ccf003c4034ab2e5015f7aec770f4
davimc <davimatosc@hotmail.com>
2022-05-11 15:18:32 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+
+
+function Form() {
+    return(
+        <h2>Form </h2>
+    )
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+function Listing() {
+
+    return(
+        <h2>Lista</h2>
+    )
+}
+
+export default Listing;
