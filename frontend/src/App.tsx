5645d9ab3da9146f57a17c2a2b7e4109c9f7a765
davimc <davimatosc@hotmail.com>
2022-05-11 16:11:38 -0300

    Form layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Listing from 'pages/Listing';
+import Listing from "pages/Listing";
 import Form from 'pages/Form';
 import Navbar from "components/Navbar";
 
