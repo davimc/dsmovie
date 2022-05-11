f8054c95fc95828964f50221795c79dd302cf46e
davimc <davimatosc@hotmail.com>
2022-05-11 17:33:46 -0300

    Routes

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,8 @@
-import { ReactComponent as Arrow } from "assets/img/arrow.svg";
 import "./styles.css"
 
 function Listing() {
     return (
-        <div className="dsmovie-pagination-container">
-            <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" disabled={true} >
-                    <Arrow />
-                </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="dsmovie-pagination-button" disabled={false} >
-                    <Arrow className="dsmovie-flip-horizontal" />
-                </button>
-            </div>
-        </div>
+        <h3>Lista</h3>
     )
 }
 
