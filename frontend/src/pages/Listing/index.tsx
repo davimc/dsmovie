9cbfde45d1a1abee6e7f6bd44f5e630cdc3f3e59
davimc <davimatosc@hotmail.com>
2022-05-11 16:51:24 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,19 @@
-function Listing() {
+import { ReactComponent as Arrow } from "assets/img/arrow.svg";
+import "./styles.css"
 
-    return(
-        <h2>Lista</h2>
+function Listing() {
+    return (
+        <div className="dsmovie-pagination-container">
+            <div className="dsmovie-pagination-box">
+                <button className="dsmovie-pagination-button" disabled={true} >
+                    <Arrow />
+                </button>
+                <p>{`${1} de ${3}`}</p>
+                <button className="dsmovie-pagination-button" disabled={false} >
+                    <Arrow className="dsmovie-flip-horizontal" />
+                </button>
+            </div>
+        </div>
     )
 }
 
